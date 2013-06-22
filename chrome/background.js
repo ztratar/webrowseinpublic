f7bf939418a4da57ce869dccf7923a386ed3dec0
Zach Tratar <backplane1@Zach-Tratars-MacBook-Air.local>
2013-06-22 16:07:18 -0700

    Only send URLs with a title

--- chrome/background.js
+++ chrome/background.js
@@ -6,10 +6,14 @@ var SERVER_PORT = 3000;
 var socket = io.connect('http://127.0.0.1:3000'),
 	user_id = null;
 
+
 chrome.storage.sync.get('uid', function(storageObj) {
 	if (!_.isEmpty(storageObj) && storageObj.uid) {
 		// User exists. Connect as that user
 		user_id = storageObj.uid;
+		socket.emit('subscribe', {
+			room: 'extension-' + user_id
+		});
 	} else {
 		// User does not exist. Let's make a new one
 		socket.on('new_user', function(data) {
@@ -17,6 +21,9 @@ chrome.storage.sync.get('uid', function(storageObj) {
 				'uid': data.id
 			}, function() {
 				user_id = data.id;
+				socket.emit('subscribe', {
+					room: 'extension-' + user_id
+				});
 			});
 		});
 		socket.emit('new_user');
@@ -26,7 +33,7 @@ chrome.storage.sync.get('uid', function(storageObj) {
 // Wrapper function that gets new page action and
 // sends it to the nodeJS backend
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (user_id) {
+	if (user_id && request.title) {
 		socket.emit(
 			'new_visit', 
 			JSON.stringify({
